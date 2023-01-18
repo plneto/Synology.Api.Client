2d09f7dfb77717799c75f9b3b29f991e99e0d75c
chesh111re <alladusheck@gmail.com>
2023-01-19 00:49:33 +0400

    Add test for create task in DownloadStationApi

--- src/Synology.Api.Client.Integration.Tests/DownloadStationApiTests.cs
+++ src/Synology.Api.Client.Integration.Tests/DownloadStationApiTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Synology.Api.Client.Apis.DownloadStation.Task.Models;
 using Synology.Api.Client.Integration.Tests.Fixtures;
 using Xunit;
 
@@ -16,7 +18,7 @@ namespace Synology.Api.Client.Integration.Tests
         }
 
         [Fact]
-        public async void DownloadStationApi_DownloadStation_ListTasks()
+        public async Task DownloadStationApi_DownloadStation_ListTasks()
         {
             // arrange && act
             var listResponse = await _fixture
@@ -27,6 +29,42 @@ namespace Synology.Api.Client.Integration.Tests
 
             // assert
             listResponse.Should().NotBeNull();
+        }
+
+        //ToDo добавить существующую ссылку
+        [Fact]
+        public async Task DownloadStationApi_DownloadStation_CreateTaskWithLogin()
+        {
+            // arrange && act
+            var request = new DownloadStationTaskCreateRequest("ftps://192.0.0.1:21/test/test.zip");
+            
+            var createResponse = await _fixture
+                .Client
+                .DownloadStationApi()
+                .TaskEndpoint()
+                .CreateAsync(request);
+            
+            // assert
+            createResponse.Should().NotBeNull();
+        }
+        
+        //ToDo указать ссылку на файл или логин и пароль
+        [Fact]
+        public async Task DownloadStationApi_DownloadStation_CreateTasksWithoutLogin()
+        {
+            // arrange && act
+            var fixture = new SynologyFixture();
+            var request = new DownloadStationTaskCreateRequest("ftps://192.0.0.1:21/test/test.zip", username: "",
+                password: "");
+            
+            var createResponse = await fixture
+                .Client
+                .DownloadStationApi()
+                .TaskEndpoint()
+                .CreateAsync(request);
+            
+            // assert
+            createResponse.Should().NotBeNull();
         }
     }
 }
