6ce2e7ca2565a5f29cdcadaa69ac793394326f90
chesh111re <alladusheck@gmail.com>
2023-01-19 00:22:28 +0400

    Add method in IDownloadStationTaskEndpoint

--- src/Synology.Api.Client/Apis/DownloadStation/Task/IDownloadStationTaskEndpoint.cs
+++ src/Synology.Api.Client/Apis/DownloadStation/Task/IDownloadStationTaskEndpoint.cs
@@ -1,10 +1,12 @@
 ﻿using System.Threading.Tasks;
 using Synology.Api.Client.Apis.DownloadStation.Task.Models;
+using Synology.Api.Client.Shared.Models;
 
 namespace Synology.Api.Client.Apis.DownloadStation.Task
 {
     public interface IDownloadStationTaskEndpoint
     {
         Task<DownloadStationTaskListResponse> ListAsync();
+        Task<BaseApiResponse> CreateAsync(DownloadStationTaskCreateRequest request);
     }
 }
