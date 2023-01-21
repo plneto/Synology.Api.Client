293daf7a54958f040b45d2aa4e7c74855c821801
Alexey Elfimov <skrech-alex@mail.ru>
2023-01-21 20:14:02 +0300

    Add class DownloadStationTaskDelete for send request

---
+++ src/Synology.Api.Client/Apis/DownloadStation/Task/Models/DownloadStationTaskDeleteRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Synology.Api.Client.Apis.DownloadStation.Task.Models
+{
+    public class DownloadStationTaskDeleteRequest
+    {
+        public string Id { get; set; }
+        
+        [JsonProperty("force_complete")]
+        public bool ForceComplete { get; set; }
+    }
+}
