68fb4261a346a9c738c3a947b45a793cf238ec4b
PeskovV <alladusheck@gmail.com>
2023-03-08 10:47:08 +0400

    Fix response class

--- src/Synology.Api.Client/Apis/DownloadStation/Task/Models/DownloadStationTaskResumeResponse.cs
+++ src/Synology.Api.Client/Apis/DownloadStation/Task/Models/DownloadStationTaskResumeResponse.cs
@@ -2,7 +2,7 @@ namespace Synology.Api.Client.Apis.DownloadStation.Task.Models
 {
     public class DownloadStationTaskResumeResponse
     {
-        public string Id { get; set; }
         public int Error { get; set; }
+        public string Id { get; set; }
     }
 }
